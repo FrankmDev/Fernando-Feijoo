f2b463c093d5dbcc18b877c6790247813c8dfdc2
FrankmDev <frankmmarindev@gmail.com>
2024-12-26 14:13:37 +0100

    'structure'

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,9 +3,14 @@ export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {
-      colors: {
-        white: "var(--white)",
-        black: "var(--black)",
+      spacing: {
+        xs: "var(--space-xs)", // 4px
+        sm: "var(--space-sm)", // 8px
+        md: "var(--space-md)", // 16px
+        lg: "var(--space-lg)", // 24px
+        xl: "var(--space-xl)", // 32px
+        "2xl": "var(--space-2xl)", // 48px
+        "3xl": "var(--space-3xl)", // 64px
       },
     },
   },
