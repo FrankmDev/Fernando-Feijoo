fc348d87a4d4e98e458e2284b46dc8c77d50778a
FrankmDev <frankmmarindev@gmail.com>
2025-04-15 22:44:40 +0200

    absolute-routes

--- src/components/SliderHero.vue
+++ src/components/SliderHero.vue
@@ -28,7 +28,7 @@ const plugin = Autoplay({
         <div class="p-1">
           <Card>
             <CardContent class="flex items-center justify-center">
-              <img src="/public/heroSlider/TheFall.avif" />
+              <img src="/heroSlider/TheFall.avif" />
             </CardContent>
           </Card>
         </div>
@@ -38,7 +38,7 @@ const plugin = Autoplay({
         <div class="p-1">
           <Card>
             <CardContent class="flex items-center justify-center">
-              <img src="/public/heroSlider/2.avif" />
+              <img src="/heroSlider/2.avif" />
             </CardContent>
           </Card>
         </div>
@@ -48,7 +48,7 @@ const plugin = Autoplay({
         <div class="p-1">
           <Card>
             <CardContent class="flex items-center justify-center">
-              <img src="/public/heroSlider/3.avif" />
+              <img src="/heroSlider/3.avif" />
             </CardContent>
           </Card>
         </div>
