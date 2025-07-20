20cab6c4d7a0b60ac4c7898932d980b89a57c06f
FrankmDev <frankmmarindev@gmail.com>
2025-07-20 13:03:33 +0200

    2

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,30 +2,11 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import vue from "@astrojs/vue";
-import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-  output: "hybrid",
-  adapter: vercel({
-    webAnalytics: { enabled: true },
-  }),
+  output: "static",
   integrations: [tailwind({ applyBaseStyles: false }), vue()],
-  vite: {
-    define: {
-      "process.env.NODE_ENV": JSON.stringify(
-        process.env.NODE_ENV || "development",
-      ),
-    },
-    optimizeDeps: {
-      exclude: ["@rollup/rollup-linux-x64-gnu"],
-    },
-    build: {
-      rollupOptions: {
-        external: ["@rollup/rollup-linux-x64-gnu"],
-      },
-    },
-  },
   server: {
     port: 4321,
     host: true,
