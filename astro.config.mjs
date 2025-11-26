5a84b0c67e1d02e371c95578335855c68065ab2c
FrankmDev <frankmmarindev@gmail.com>
2025-11-26 07:44:50 +0100

    changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,15 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import vue from "@astrojs/vue";
+import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://fernando-feijoo.com",
   output: "static",
+  adapter: node({
+    mode: "standalone",
+  }),
   integrations: [tailwind({ applyBaseStyles: false }), vue()],
   vite: {
     ssr: {
